d4eed4e7ba5da3d1dd0647e0cf31bd299b1c91ac
Pedro Basilio <artiumdominus@protonmail.com>
2019-10-25 18:43:05 -0300

    Login

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,81 @@
 var express = require('express');
+var session = require('express-session');
+const MongoClient = require('mongodb').MongoClient;
+
 var router = express.Router();
+
+router.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// router.use(bodyParser.urlencoded({extended : true}));
+// router.use(bodyParser.json());
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database name
+const dbName = 'bookmarks';
+
+// Database config
+const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
 var api_rest = require('./api_rest');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.session)
+  console.log(req.session.loggedin)
+  if (req.session.loggedin) {
+    res.redirect('/bookmarks/')
+  } else {
+    res.redirect('/login/')
+  }
+  //res.render('index', { title: 'Express' });
 });
 
 router.get('/login/', function (req, res) {
-  res.send('Login screen');
+  if (!req.session.loggedin) {
+    res.render('login')
+  } else {
+    res.redirect('/bookmarks/')
+  }
 });
+
+router.post('/login/auth', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username && password) {
+    // TODO
+    MongoClient.connect(url, dbConfig, function (err, client) {
+      if (err) throw err;
+    
+      const db = client.db(dbName);
+      
+      const accounts = db.collection('accounts');
+      
+      accounts.find({$and: [{'username' : username}, {'password' : password}]}).toArray((err, docs) => {
+        if (err) throw err;
+        
+        if (docs.length > 0) {
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect('/bookmarks/');
+        } else {
+          res.send('Incorrect Username and/or Password!');
+        }
+        res.end();
+      });
+    });
+  } else {
+    res.send('Please enter Username and Password!');
+    res.end();
+  }
+})
 
 router.get('/bookmarks/', function (req, res) {
   if (typeof req.query.id === "undefined") {
